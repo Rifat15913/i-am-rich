a235810d7a08ca2529bcaa0c4f47a164d3d1d67e
Rifat <rifat@setuptech.international>
2020-01-02 18:13:38 +0600

    Change the image asset

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-          backgroundColor: Colors.blueGrey,
+          backgroundColor: Colors.cyan,
           appBar: AppBar(
-            title: Text("I Am Rich"),
+            title: Text("I Am Poor"),
             centerTitle: true,
-            backgroundColor: Colors.blueGrey[900],
+            backgroundColor: Colors.teal[900],
           ),
           body: Center(
             child: Image(
-              image: AssetImage('images/diamond.png'),
+              image: AssetImage('images/coal.png'),
             ),
           ),
         ),
